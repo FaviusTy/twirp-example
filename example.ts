6b2ea91a108ae3d3989e42eb0d3b30f6b21cbc88
Favi_ty <favius983@gmail.com>
2019-02-12 23:42:05 +0900

    test type utility

---
+++ example.ts
@@ -0,0 +1,26 @@
+import "./outputs/sample.pb.d";
+import { sample } from "./outputs/sample.pb.d";
+
+interface Example {
+  name: string | null | undefined;
+  arr?: Example[] | null;
+}
+type Unwrap<T> = T extends { [K: string]: infer U }
+  ? U
+  : T extends Array<infer U>
+  ? U
+  : T;
+
+type isPrimitive<T> = T extends Unwrap<T> ? T : never;
+
+type Diff<T, U> = T extends U ? never : T;
+
+type DeepNonNullable<T> = {
+  [P in keyof T]-?: T[P] extends isPrimitive<T[P]>
+    ? Diff<T[P], null | undefined>
+    : DeepNonNullable<T[P]>
+};
+
+type SafeExample = DeepNonNullable<Example>;
+
+type SafeRepeated = DeepNonNullable<sample.Sample.IRepeated>;
